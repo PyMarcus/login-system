6ae4b1687e9a9ba1bdc902a35d0ed30538ceae4f
Marcus Vinícius Ribeiro Andrade <88283829+PyMarcus@users.noreply.github.com>
2021-10-25 07:09:00 -0300

    Update script.js

--- Login_System/assets/JS/script.js
+++ Login_System/assets/JS/script.js
@@ -2,17 +2,25 @@
 login = ""
 pw = ""
 
+//login button
 function captureData(){
     login = document.getElementById("ln").value
     pw = document.getElementById("pw").value
-    if(login != '' || login != ' '){
+    console.log(login)
+    if(login != '' && login != ' '){
       alert('Login efetuado com sucesso!')
     document.getElementById('body').style.background = 'black'
     document.getElementById('main-box').style.display = 'none'
     document.getElementById('forlogin').innerHTML = 'Bem vindo, ' + login + "!"
-    }
+    document.getElementById('forlogin').style.color = "white"
+    document.getElementById('forlogin').style.textAlign = 'center'
+    document.getElementById('forlogin').style.fontSize = '10vh'
+    document.getElementById('forlogin').style.marginTop = '18%'
+
 
   }
+}
+//cancel button
 function abort(){
   window.open('https://github.com/PyMarcus')
   window.close()
